9b435aadf096d8dd3b3eec444476cf4e2413d2e2
taichi tashiro <taichi@taichinoMacBook-Air.local>
2023-10-09 16:32:35 +0900

    exit

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -6,7 +6,21 @@
 /*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 20:35:00 by tatashir          #+#    #+#             */
-/*   Updated: 2023/10/08 22:23:10 by tatashir         ###   ########.fr       */
+/*   Updated: 2023/10/09 16:32:28 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	builtin_exit(char *argv[])
+{
+	int	status;
+
+	ft_putendl_fd(MSG_EXIT, STDOUT_FILENO);
+	if (argv == NULL || argv[0] == NULL || argv[1] == NULL)
+		status = g_shell.status;
+	else if (argv[2] == NULL)
+	{
+		if ()
+	}
+}
