71c7d5aabf970975af1dcced0d4a85f1c1cd0e3d
Taichi Tashiro <tatashir@c1r16s7.42tokyo.jp>
2023-10-10 20:41:55 +0900

    init

---
+++ src/init.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 20:35:53 by tatashir          #+#    #+#             */
+/*   Updated: 2023/10/10 20:41:43 by tatashir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_shell	g_shell;
+
+static t_list	*tlst_from_strlst(char *envp[])
+{
+	t_list	head;
+	t_list	*cur;
+	char	*content;
+	size_t	i;
+
+	head.next = NULL;
+	cur = &head;
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		content = ft_strdup(envp[i]);
+		if (content == NULL)
+			exit(EXIT_FAILURE);
+		cur->next = ft_lstnew(content);
+		if (cur->next == NULL)
+			exit(EXIT_FAILURE);
+		cur = cur->next;
+		i++;
+	}
+	return (head.next);
+}
+
+void	init_global(char *envp[])
+{
+	g_shell.environ = tlst_from_strlst(envp);
+	g_shell.status = 0;
+	g_shell.cmd = NULL;
+	g_shell.kill_child = false;
+	g_shell.heredoc_sigint = false;
+}
