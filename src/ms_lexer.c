341baa6c8113bf839927de18050a086a0ccadad2
Taichi Tashiro <tatashir@c1r16s5.42tokyo.jp>
2023-10-12 00:03:32 +0900

    lexer

---
+++ src/ms_lexer.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_lexer.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 20:36:21 by tatashir          #+#    #+#             */
+/*   Updated: 2023/10/12 00:02:58 by tatashir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static size_t	ms_lexer_tokensize(char *line)
+{
+	size_t	size;
+	size_t	pos;
+	size_t	len;
+
+	if (line == NULL)
+		return (0);
+	size = 0;
+	pos = 0;
+	while (*(line + pos) != '\0')
+	{
+		while (*(line + pos) == CHRS_DELIM[0])
+			pos++;
+		len = ms_lexer_tokenlen(line + pos);
+		if (len > 0)
+			size++;
+		pos += len;
+	}
+	return (size);
+}
+
+t_token	*ms_lexer(char *line)
+{
+	t_token	*token;
+	size_t	size;
+
+	size = ms_lexer_tokensize(line);
+	token = (t_token *)malloc((size + 1) * sizeof(t_token));
+	if (token == NULL)
+		exit(ENOMEM);
+	ms_lexer_gettoken(token, line);
+	return (token);
+}
