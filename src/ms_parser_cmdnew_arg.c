030aa5bd0e168432d81ad0ecb224e823aa1ecc6c
Taichi Tashiro <tatashir@c1r16s5.42tokyo.jp>
2023-10-12 00:22:06 +0900

    cmdnewarg

---
+++ src/ms_parser_cmdnew_arg.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_parser_cmdnew_arg.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 20:36:54 by tatashir          #+#    #+#             */
+/*   Updated: 2023/10/12 00:21:30 by tatashir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static size_t	ms_parser_cmdnew_arg_size(t_token *token, size_t idx)
+{
+	size_t	size;
+
+	size = 0;
+	while (token[idx].str != NULL && token[idx].flag != FLAG_PIPE)
+	{
+		if (token[idx].flag == FLAG_STRING)
+			size++;
+		else
+			idx++;
+		if (token[idx].str != NULL && token[idx].flag != FLAG_PIPE)
+			idx++;
+	}
+	return (size);
+}
+
+char	**ms_parser_cmdnew_arg(t_token *token, size_t i_token)
+{
+	char	**arg;
+	size_t	size;
+	size_t	i_arg;
+
+	size = ms_parser_cmdnew_arg_size(token, i_token);
+	arg = (char **)malloc((size + 1) * sizeof(char *));
+	if (arg == NULL)
+		return (NULL);
+	i_arg = 0;
+	while (token[i_token].str != NULL && token[i_token].flag != FLAG_PIPE)
+	{
+		if (token[i_token].flag == FLAG_STRING)
+			arg[i_arg++] = token[i_token].str;
+		else
+			i_token++;
+		if (token[i_token].str != NULL && token[i_token].flag != FLAG_PIPE)
+			i_token++;
+	}
+	arg[size] = NULL;
+	return (arg);
+}
