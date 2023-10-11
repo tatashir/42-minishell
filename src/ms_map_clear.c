0227afc3d9a00ded9ea6116529cd64687f4b4bf2
Taichi Tashiro <tatashir@c1r16s5.42tokyo.jp>
2023-10-12 00:06:35 +0900

    map-clear

---
+++ src/ms_map_clear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_map_clear.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 20:36:38 by tatashir          #+#    #+#             */
+/*   Updated: 2023/08/07 20:36:38 by tatashir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	*ms_map_clear(char **map, size_t size)
+{
+	size_t	i;
+
+	if (map == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+		free(map[i++]);
+	free(map);
+	map = NULL;
+	return (NULL);
+}
