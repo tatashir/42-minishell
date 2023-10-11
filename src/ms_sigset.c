fb78c709e9e10d5b12990d31f5170e35767e8b71
Taichi Tashiro <tatashir@c1r16s5.42tokyo.jp>
2023-10-12 00:31:53 +0900

    signal-set

---
+++ src/ms_sigset.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_sigset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 20:37:33 by tatashir          #+#    #+#             */
+/*   Updated: 2023/08/07 20:37:34 by tatashir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ms_sigset_rl(void)
+{
+	t_sa	sa;
+
+	ft_bzero(&sa, sizeof(t_sa));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ms_sighandler_rl;
+	sigaction(SIGINT, &sa, NULL);
+}
+
+void	ms_sigset_noquit(void)
+{
+	t_sa	sa;
+
+	ft_bzero(&sa, sizeof(t_sa));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &sa, NULL);
+}
+
+void	ms_sigset_rl_heredoc(void)
+{
+	t_sa	sa;
+
+	ft_bzero(&sa, sizeof(t_sa));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ms_sighandler_rl_heredoc;
+	sigaction(SIGINT, &sa, NULL);
+}
+
+void	ms_sigset_exec(void)
+{
+	t_sa	sa;
+
+	ft_bzero(&sa, sizeof(t_sa));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ms_sighandler_exec;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+}
