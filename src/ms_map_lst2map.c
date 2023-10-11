727bf08f346d3a0e074c6f04bd349f922c7f7412
Taichi Tashiro <tatashir@c1r16s5.42tokyo.jp>
2023-10-12 00:20:02 +0900

    lst-to-map

---
+++ src/ms_map_lst2map.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_map_lst2map.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 20:36:43 by tatashir          #+#    #+#             */
+/*   Updated: 2023/08/07 20:36:43 by tatashir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	**ms_map_lst2map(t_list *lst)
+{
+	char	**map;
+	t_list	*ele;
+	size_t	size;
+	size_t	i;
+
+	size = ft_lstsize(lst);
+	ele = lst;
+	map = (char **)malloc((size + 1) * sizeof(char *));
+	if (map == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		map[i] = ele->content;
+		ele = ele->next;
+		i++;
+	}
+	map[i] = NULL;
+	return (map);
+}
