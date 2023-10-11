bd7e4ef5fd5dfa75ab728be0944681c24cf664be
Taichi Tashiro <tatashir@c1r16s5.42tokyo.jp>
2023-10-12 00:31:22 +0900

    signal

---
+++ src/ms_sighandler.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_sighandler.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 20:37:28 by tatashir          #+#    #+#             */
+/*   Updated: 2023/08/14 01:26:42 by tatashir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ms_sighandler_rl(int signum, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void)info;
+	if (signum == SIGQUIT)
+		ms_builtin_exit(NULL);
+	if (signum == SIGINT)
+	{
+		g_shell.status = 1;
+		rl_replace_line("", 0);
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+void	ms_sighandler_rl_heredoc(int signum, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void)info;
+	if (signum == SIGINT)
+	{
+		g_shell.status = 1;
+		g_shell.heredoc_sigint = true;
+	}
+}
+
+void	ms_sighandler_exec(int signum, siginfo_t *info, void *context)
+{
+	t_cmd	*cur;
+
+	(void)context;
+	(void)info;
+	if (signum == SIGQUIT || signum == SIGINT)
+	{
+		g_shell.status = 128 + signum;
+		g_shell.kill_child = true;
+		cur = g_shell.cmd;
+		while (cur != NULL)
+		{
+			kill(cur->pid, signum);
+			cur = cur->next;
+		}
+		if (signum == SIGQUIT)
+			ft_putendl_fd(MSG_SIG_QUIT, STDOUT_FILENO);
+		if (signum == SIGINT)
+			ft_putchar_fd('\n', STDOUT_FILENO);
+	}
+}
